dbb0eb3ce5fc226d6527e92272e6cf5e365984ee
Fernando Miron <fernando.miron@gmail.com>
2020-04-05 11:46:50 +0200

    commented the type.py file

--- reservations/app/models/type.py
+++ reservations/app/models/type.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+"""
+type model
+"""
 class Type(models.Model):
     type = models.CharField(max_length=60)
 
@@ -7,6 +10,6 @@ class Type(models.Model):
         db_table = 'types'
         verbose_name_plural = 'Types'
         verbose_name = 'Type'
-        
+
     def __str__(self):
         return self.type
