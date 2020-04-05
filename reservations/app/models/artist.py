74aca0c6aef8fc9dffc6efbf072b89b955b3ef6f
Likiol <lionel.soupart@gmail.com>
2020-04-05 21:51:22 +0200

    modif ignore +artist

--- reservations/app/models/artist.py
+++ reservations/app/models/artist.py
@@ -5,3 +5,18 @@ class Artist (models.Model):
 
     firstname= models.CharField(max_length = 60)
     lastname = models.CharField(max_length = 60)
+
+    class Meta:
+        """meta for Artist."""
+
+        verbose_name = "Artist"
+        verbose_name_plural = "Artists"
+
+        ordering = ['pk']
+
+    
+    def __str__(self) :
+        """representation of MODELNAME"""
+
+        return "({}) {} {}".format(self.pk, self.firstname, self.lastname)
+
