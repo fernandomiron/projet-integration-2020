9e222b8b1167f9bd92ec17998ca59ca5752abf5a
Fernando Miron <fernando.miron@gmail.com>
2020-04-05 17:37:39 +0200

    created show model + import into init.py

--- reservations/app/models/__init__.py
+++ reservations/app/models/__init__.py
@@ -5,3 +5,4 @@ from .type import *
 from .role import *
 from .locality import *
 from .location import *
+from .show import *
---
+++ reservations/app/models/show.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Show(models.Model):
+    slug = models.CharField(unique=True, max_length=60)
+    title = models.CharField(max_length=255)
+    poster_url = models.CharField(max_length=255)
+    location_id = models.ForeignKey('Location',  on_delete=models.SET_NULL, null=True)
+    bookable = models.BooleanField()
+    price=models.FloatField()
+
+    class Meta:
+        db_table = 'shows'
+        verbose_name_plural = 'Shows'
+        verbose_name = 'Show'
+
+    def __str__(self):
+        return 'Title : ' + self.title \
+            + '\nPoster URL : ' + self.title \
+            + '\nLocation ID : ' + self.location_id \
+            + '\nBookable : ' + self.bookable \
+            + '\nPrice : ' + self.price
+                  
