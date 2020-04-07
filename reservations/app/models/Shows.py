2cf9a921dc69da7e7072ef1e3ca8d163110bc23b
Likiol <lionel.soupart@gmail.com>
2020-04-07 21:18:26 +0200

    update shows model

--- reservations/app/models/Shows.py
+++ reservations/app/models/Shows.py
@@ -3,13 +3,14 @@ from django.db import models
 class Shows (models.Model) : 
     """model definition of Shows"""
 
-    slug = models.CharField(max_length = 60)
+    slug = models.CharField(max_length = 60, unique=True)
     title = models.CharField(max_length = 255)
     poster_url = models.CharField(max_length =255)
-    bookable = models.BooleanField
-    price = models.IntegerField
+    bookable = models.BooleanField()
+    price = models.FloatField()
     description = models.CharField(max_length = 1000)
-    created_at = models.DateField
+    created_at = models.DateField()
+    """Location_id ==>foreign Key, comment?"""
 
 
     class Meta:
