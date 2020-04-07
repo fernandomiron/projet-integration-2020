d1f55927639966fb8a2cf70f2e88a2ca58f5f5b8
Dieudonné <twahirwa25@gmail.com>
2020-04-08 01:44:04 +0200

    update artist model

--- reservations/app/models/artist.py
+++ reservations/app/models/artist.py
@@ -2,12 +2,15 @@ from django.db import models
 
 class Artist(models.Model):
 
+    """ Artist model definition """
     firstname = models.CharField(max_length=60)
     lastname = models.CharField(max_length=60)
 
+    """ Artist meta definition """
     class Meta:
         verbose_name = "Artist"
         verbose_name_plural = "Artists"
 
+    """ String representation of Artist """
     def __str__(self):
-        return f'{self.pk} {self.lastname} {self.firstname}'
+        return f'({self.pk}) {self.lastname} {self.firstname}'
