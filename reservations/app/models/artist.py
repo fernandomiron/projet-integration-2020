e1735ee54b32009e634b9ec64200ae5a49f01988
Dieudonné <twahirwa25@gmail.com>
2020-04-07 22:57:46 +0200

    update artist model

--- reservations/app/models/artist.py
+++ reservations/app/models/artist.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.urls import reverse
 
 class Artist(models.Model):
 
@@ -7,12 +6,8 @@ class Artist(models.Model):
     lastname = models.CharField(max_length=60)
 
     class Meta:
-        db_table = "artists"
         verbose_name = "Artist"
         verbose_name_plural = "Artists"
 
     def __str__(self):
-        return self.firstname + ' ' + self.lastname
-
-    def get_absolute_url(self):
-        return reverse("Artist_detail", kwargs={"pk": self.pk})
+        return f'{self.pk} {self.lastname} {self.firstname}'
