f74a161bbec63ab397a363693bc1dc3961ce2874
Dieudonné <twahirwa25@gmail.com>
2020-04-08 01:44:58 +0200

    update show model

--- reservations/app/models/show.py
+++ reservations/app/models/show.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.urls import reverse
 from datetime import date
 
 class Show(models.Model):
 
+    """ Show model definition """
     location_id = models.ForeignKey("Location", on_delete=models.SET_NULL, null=True)
     slug = models.SlugField(max_length=60, unique=True)
     title = models.CharField(max_length=255)
@@ -13,19 +13,11 @@ class Show(models.Model):
     description = models.TextField()
     created_at = models.DateField(auto_now=False, auto_now_add=False, default=date.today, null=True)
 
+    """ Show meta definition """
     class Meta:
-        db_table = "shows"
         verbose_name = "Show"
         verbose_name_plural = "Shows"
 
+    """ String representation of Show """
     def __str__(self):
-        return 'Title : ' + self.title \
-                + '\nSlug : ' + self.slug \
-                + '\nPoster URL : ' + self.poster_url \
-                + '\nBookable : ' + self.bookable \
-                + '\nPrice : ' + self.price \
-                + '\nDescription : ' + self.description \
-                + '\nCreated at : ' + self.created_at 
-
-    def get_absolute_url(self):
-        return reverse("Show_detail", kwargs={"pk": self.pk})
+        return f'({self.pk}) {self.title}'
