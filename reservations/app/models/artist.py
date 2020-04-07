7bbb430272d9bccf1e9e728499d1d55124d6fb56
dreamer_658 <calebyambu@yahoo.fr>
2020-04-07 20:08:22 +0200

    add meta data and str method on Artist

--- reservations/app/models/artist.py
+++ reservations/app/models/artist.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 class Artist(models.Model):
-    """ One artist's model """
+    """ Artist model """
     firstname = models.CharField(max_length=60)
     lastname = models.CharField(max_length=60)
+
+    class Meta:
+        """ Artist model meta data """
+        verbose_name = "Artist"
+        verbose_name_plural = "Artists"
+        ordering = ['firstname', 'lastname', 'pk']
+
+    def __str__(self):
+        """ Overloading __str__ method"""
+        return " {} {} {}".format(self.pk, self.firstname, self.lastname)
