cbf0d30dc3256762ffe7f881b746b2be3081a54b
Dieudonné <twahirwa25@gmail.com>
2020-04-08 01:43:55 +0200

    update artist_type model

--- reservations/app/models/artist_type.py
+++ reservations/app/models/artist_type.py
@@ -2,12 +2,15 @@ from django.db import models
 
 class Artist_type(models.Model):
 
+    """ Artist_type model definition """
     artist_id = models.ForeignKey("Artist", on_delete=models.CASCADE)
     type_id = models.ForeignKey("Type", on_delete=models.CASCADE)
 
+    """ Artist_type meta definition """
     class Meta:
         verbose_name = "Artist_type"
         verbose_name_plural = "Artist_types"
 
+    """ String representation of Artist_type """
     def __str__(self):
-        return f'Artist : {self.artist_id}, Type :  {self.type_id}'
+        return f'({self.pk}) Artist : {self.artist_id}, Type :  {self.type_id}'
