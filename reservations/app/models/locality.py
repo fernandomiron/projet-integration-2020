e0c427eefad7e597a9cda014cd456da0444d8e3d
Dieudonné <twahirwa25@gmail.com>
2020-04-08 01:44:29 +0200

    update locality model

--- reservations/app/models/locality.py
+++ reservations/app/models/locality.py
@@ -3,16 +3,16 @@ from django.urls import reverse
 
 class Locality(models.Model):
 
+    """ Locality model definition """
     postal_code = models.CharField(max_length=6, unique=True)
     locality = models.CharField(max_length=60)
 
+    """ Locality meta definition """
     class Meta:
-        db_table = "localities"
         verbose_name = "Locality"
         verbose_name_plural = "Localities"
 
+    """ String representation of Locality """
     def __str__(self):
-        return self.postal_code + ' ' + self.locality
+        return f'({self.pk}) {self.postal_code} {self.locality}'
 
-    def get_absolute_url(self):
-        return reverse("Locality_detail", kwargs={"pk": self.pk})
