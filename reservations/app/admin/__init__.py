bc252f21b5d3543ec79795d6ae9752af6bfc4511
Fernando Miron <fernando.miron@gmail.com>
2020-04-09 22:41:28 +0200

    added locality to admin view

--- reservations/app/admin/__init__.py
+++ reservations/app/admin/__init__.py
@@ -1,2 +1,4 @@
 from app.admin.artist import *
 from app.admin.artist_type import *
+from app.admin.collaboration import *
+from app.admin.locality import *
---
+++ reservations/app/admin/locality.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+from app.models.locality import Locality
+admin.site.register(Locality)
