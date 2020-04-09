0d5043c0a55b9e24b631b04cbdcb38d85fc62e33
Dieudonné <twahirwa25@gmail.com>
2020-04-09 11:58:09 +0200

    update app models : create user_profile

---
+++ reservations/app/models/user_profile.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+from .role import Role
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role_id = models.ForeignKey(Role, on_delete=models.CASCADE)
+    langue = models.CharField(max_length=2)
+
+    class Meta:
+        """Meta definition for UserProfile."""
+
+        verbose_name = 'User\'s profile'
+        verbose_name_plural = 'User\'s profiles'
+
+    def __str__(self):
+        """Unicode representation of UserProfile."""
+        return f'({self.pk}) Profile n° ({self.user.pk}) - {self.user.username}"'
