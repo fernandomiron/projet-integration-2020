c7a24802745482cd8185a3e456918814e08c3081
Dieudonné <twahirwa25@gmail.com>
2020-04-15 19:03:16 +0200

    update app models ; update RepresentationUser class

--- reservations/app/models/representation_user.py
+++ reservations/app/models/representation_user.py
@@ -1,18 +1,22 @@
 from django.db import models
 from django.urls import reverse
 
+
 class RepresentationUser(models.Model):
 
-    """ RepresentationUser model definition """
-    show_id = models.ForeignKey("Locality", on_delete=models.SET_NULL, null=True)
-    user_id = models.ForeignKey("UserProfile", on_delete=models.SET_NULL, null=True)
+    """ Representation_user model definition """
+    user_id = models.ForeignKey("UserProfile", on_delete=models.CASCADE)
+    representation_id = models.ForeignKey("Representation", on_delete=models.CASCADE)
     places = models.IntegerField()
-
-    """ RepresentationUser meta definition """
+    
+    """ Representation_user meta definition """
     class Meta:
-        verbose_name = "RepresentationUser"
+        verbose_name = "Representation_user"
         verbose_name_plural = "Representation_users"
 
-    """ String RepresentationUser of RepresentationUser """
+    """ String Representation_user of Representation_user """
     def __str__(self):
         return f'({self.pk})'
+
+    def get_absolute_url(self):
+        return reverse("app:representation_user-detail", kwargs={"pk": self.pk})
