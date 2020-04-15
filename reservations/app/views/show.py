62b7befe871035f69e55bd8e434e2faaac05ae46
Dieudonné <twahirwa25@gmail.com>
2020-04-15 19:57:02 +0200

    update app view : create show detail view

--- reservations/app/views/show.py
+++ reservations/app/views/show.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from app.models import Show
 
+# Show ListView Class definition
 class ListView(ListView):
     model = Show
     template_name = 'app/show/index.html'
@@ -13,4 +14,15 @@ class ListView(ListView):
     def get_context_data(self, **kwargs):
         context = super(ListView, self).get_context_data(**kwargs)
         context['title'] = "Liste des spectacles"
+        return context
+
+# Show DetailView Class definition
+class DetailView(DetailView):
+    model = Show
+    template_name = 'app/show/detail.html'
+    context_object_name = 'show'
+
+    def get_context_data(self, **kwargs):
+        context = super(DetailView, self).get_context_data(**kwargs)
+        context['title'] = f"show n°{ self.Show.pk }"
         return context
