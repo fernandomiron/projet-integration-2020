6f986cd1e1cba4273207b4eea6a6de2d47046cf5
Likiol <lionel.soupart@gmail.com>
2020-04-15 21:32:28 +0200

    add view booking

--- reservations/app/urls.py
+++ reservations/app/urls.py
@@ -5,9 +5,11 @@ from django.urls import path
 from .views import views
 from .views import shows
 from .views import show
+from .views import book
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),  # Test homepage
     url(r'^shows_list',shows.shows_list),
     url(r'^shows/(?P<id_article>.+)', show.show),
+    url(r'^booking',book.book)
 ]
---
+++ reservations/app/views/book.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def book (request) :
+
+    return HttpResponse (
+    "vue permettant de réserver un show" )
