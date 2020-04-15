15224a94c7027a555f98ebc91ba8c6d9ae7073b3
Dieudonné <twahirwa25@gmail.com>
2020-04-15 19:02:45 +0200

    update app models : update Representation model

--- reservations/app/models/representation.py
+++ reservations/app/models/representation.py
@@ -1,13 +1,13 @@
 from django.db import models
-from django.urls import reverse
 
 
 class Representation(models.Model):
 
     """ Representation model definition """
-    show_id = models.ForeignKey("Locality", on_delete=models.SET_NULL, null=True)
+    show_id = models.ForeignKey("Locality", on_delete=models.CASCADE)
     when = models.DateField(auto_now=False, auto_now_add=False)
-    location_id = models.ForeignKey("Location", on_delete=models.SET_NULL, null=True)
+    location_id = models.ForeignKey("Location", on_delete=models.CASCADE)
+    users = models.ManyToManyField("UserProfile", through='RepresentationUser')
     """ Representation meta definition """
     class Meta:
         verbose_name = "Representation"
