94b4dcdd54dfe913c209a9aef19e3cf8dbcd3322
Dieudonné <twahirwa25@gmail.com>
2020-04-15 19:16:58 +0200

    update app urls : create shows-list url

--- reservations/app/urls.py
+++ reservations/app/urls.py
@@ -1,8 +1,12 @@
+from django.urls import re_path, include
 from django.conf.urls import url
 
 from .views import views
 
+from app.views import show
 
+app_name = 'app'
 urlpatterns = [
     url(r'^$', views.home, name='home'),  # Test homepage
+    re_path(r'^shows-index/$', show.ListView.as_view(), name='shows-list'), # ShowList page
 ]
