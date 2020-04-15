6f46da78fdcae2dfdbde8aef2536470aeb0f44fc
Dieudonné <twahirwa25@gmail.com>
2020-04-15 21:00:55 +0200

    update app urls : fix re_path

--- reservations/app/urls.py
+++ reservations/app/urls.py
@@ -6,8 +6,8 @@ from app.views import views, show
 
 app_name = 'app'
 urlpatterns = [
-    re_path(r'^$', views.HomeView.as_view(), name='home'),  # Homepage
+    re_path(r'home$', views.HomeView.as_view(), name='home'),  # Homepage
 
-    re_path(r'^shows-index/$', show.ListView.as_view(), name='shows-list'), # ShowList page
-    re_path(r'^show/(?P<pk>[0-9]+)/$', show.DetailView.as_view(), name='show-detail'), # ShowDetail page
+    re_path(r'shows-index/$', show.ListView.as_view(), name='shows-list'), # ShowList page
+    re_path(r'show/(?P<pk>[0-9]+)/$', show.DetailView.as_view(), name='show-detail'), # ShowDetail page
 ]
