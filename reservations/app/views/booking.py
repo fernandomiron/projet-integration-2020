3270cd4414a17975b8129b26948461d13a8e8879
Likiol <lionel.soupart@gmail.com>
2020-04-15 21:37:10 +0200

    modif booking

--- reservations/app/views/booking.py
+++ reservations/app/views/booking.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def book (request) :
+def booking (request) :
 
     return HttpResponse (
     "vue permettant de réserver un show" )
