135947dc454a6c262565f3c1d02c63d642ef5504
Dieudonné <twahirwa25@gmail.com>
2020-04-15 19:56:26 +0200

    update app views : update views.py, create HomeView class

--- reservations/app/views/views.py
+++ reservations/app/views/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
-def home(request):
+class HomeView(TemplateView):
     """Default test homepage with base-template"""
+    template_name = 'app/home.html'
 
-    return render(request, 'app/base.html', {})
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['title'] = 'Accueil'
+        return context
