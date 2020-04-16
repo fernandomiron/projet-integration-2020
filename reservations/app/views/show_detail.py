88a7d485481bf998bd2044936abab4f2702b9dbb
Likiol <lionel.soupart@gmail.com>
2020-04-16 10:30:06 +0200

    create structure template booking + show_detail + shows_list

--- reservations/app/views/show_detail.py
+++ reservations/app/views/show_detail.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 def show (request, id_Shows) :
 
--- reservations/app/views/shows_list.py
+++ reservations/app/views/shows_list.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 
 def shows_list(request):
 
-    return HttpResponse (
-    "vue comprenant l'ensemble des shows" )
+    return render (request,'app/shows_list.html',{})
 
 
