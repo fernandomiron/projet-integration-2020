7faff6dfac3a92a0c19b71ac993f89680acd019c
Dieudonné <twahirwa25@gmail.com>
2020-04-16 20:33:27 +0200

    fix merge into branch master

--- reservations/scripts/runserver.sh
+++ reservations/scripts/runserver.sh
@@ -3,7 +3,6 @@
 # Automatically restart the runserver_plus process if it stops with an error code
 # This is for development purposes
 
-
 while true; do
   if ! python3 manage.py runserver_plus 0.0.0.0:8000; then
     echo "\nrunserver_plus exited with a non 0 status: restarting in 5 seconds...\n"
