4f40ddab9e299bb62be626fe5f8c2f50dfc173ab
cedric <cndjoni@gmail.com>
2020-04-16 10:27:03 +0200

    add representation models fix

--- reservations/app/models/representation.py
+++ reservations/app/models/representation.py
@@ -1,7 +1,7 @@
 from django.db import models
 from datetime import datetime
 
-class representation(models.Model):
+class Representation(models.Model):
     show_id = models.ForeignKey('Show', on_delete=models.SET_NULL, null=True)
     when = models.DateTimeField(help_text="Representation Date", null=True)
     location_id = models.ForeignKey("Location", on_delete=models.SET_NULL, null=True)
