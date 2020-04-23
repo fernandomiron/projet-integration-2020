e38e7570c297c910755a14077cfeeb97ecfa0d93
Dieudonné <twahirwa25@gmail.com>
2020-04-23 18:09:23 +0200

    update app views : create RepresentationListView

--- reservations/app/views/representation.py
+++ reservations/app/views/representation.py
@@ -1,8 +1,22 @@
-from django.views.generic import DetailView
+from django.views.generic import ListView, DetailView
 
 from app.models import Representation
 
-# Show DetailView Class definition
+
+# representation ListView Class definition
+class ListView(ListView):
+    model = Representation
+    template_name = 'app/representation/index.html'
+    context_object_name = 'representations'
+    ordering = ['when']
+    paginate_by = 9
+
+    def get_context_data(self, **kwargs):
+        context = super(ListView, self).get_context_data(**kwargs)
+        context['title'] = "Liste des prochains spectacles"
+        return context
+
+# Representation DetailView Class definition
 class DetailView(DetailView):
     model = Representation
     template_name = 'app/representation/detail.html'
@@ -11,3 +25,5 @@ class DetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super(DetailView, self).get_context_data(**kwargs)
         return context
+
+# 
