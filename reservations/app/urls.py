426c81d0796f0bb2a5483c0ef4179b0ed37c7d78
Dieudonné <twahirwa25@gmail.com>
2020-04-23 14:10:03 +0200

    update app urls : create representation-detail urls

--- reservations/app/urls.py
+++ reservations/app/urls.py
@@ -2,7 +2,7 @@ from django.urls import re_path, include
 
 from .views import views
 
-from app.views import views, show
+from app.views import views, show, representation
 
 app_name = 'app'
 urlpatterns = [
@@ -10,4 +10,6 @@ urlpatterns = [
 
     re_path(r'shows-index/$', show.ListView.as_view(), name='shows-list'), # ShowList page
     re_path(r'show/(?P<pk>[0-9]+)/$', show.DetailView.as_view(), name='show-detail'), # ShowDetail page
+
+    re_path(r'representation/(?P<pk>[0-9]+)/$', representation.DetailView.as_view(), name='representation-detail'), # RepresentationDetail page
 ]
