835159155bfd10223c5443ddc00d13bd02e33a35
Likiol <lionel.soupart@gmail.com>
2020-04-23 15:54:25 +0200

    final template view

--- reservations/app/views/booking.py
+++ reservations/app/views/booking.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from app.models import *
 
-def booking (request) :
+def booking(request):
 
     return render(request,'app/booking.html')
--- reservations/app/views/show_detail.py
+++ reservations/app/views/show_detail.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from datetime import datetime
 from app.models.show import Show
 
 def show_detail(request): 
