729796235a5e76ea47483c8aa94dd2225d5abd4b
Dieudonné <twahirwa25@gmail.com>
2020-04-26 03:11:48 +0200

    update app : update home views

--- reservations/app/views/views.py
+++ reservations/app/views/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from .show import *
+
 
 class HomeView(TemplateView):
     """Default test homepage with base-template"""
@@ -9,4 +11,5 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
         context['title'] = 'Accueil'
+        context['shows'] = Show.objects.all()
         return context
