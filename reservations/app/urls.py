9822a39098c1ef72c9e541983e9afdabaeda5c32
Dieudonné <twahirwa25@gmail.com>
2020-04-26 02:41:48 +0200

    update app urls : create login and logout urls

--- reservations/app/urls.py
+++ reservations/app/urls.py
@@ -1,12 +1,15 @@
 from django.urls import re_path, include
+from django.contrib.auth import views as auth_views
 
 from .views import views
-
-from app.views import views, show, representation
+from app.views import views, show, representation, user
 
 app_name = 'app'
 urlpatterns = [
     re_path(r'home$', views.HomeView.as_view(), name='home'),  # Homepage
+
+    re_path(r'login/$', user.CustomLoginView.as_view(), name='login' ), #User login page
+    re_path(r'^logout/', auth_views.LogoutView.as_view(template_name='app/user/logout.html'), name='logout'), #User logout page
 
     re_path(r'shows-index/$', show.ListView.as_view(), name='shows-list'), # ShowList page
     re_path(r'show/(?P<pk>[0-9]+)/$', show.DetailView.as_view(), name='show-detail'), # ShowDetail page
