5fe39d6512c3f9fe58e43502c421bed81b9a3c72
Likiol <lionel.soupart@gmail.com>
2020-04-28 19:36:37 +0200

    db

--- reservations/app/urls.py
+++ reservations/app/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from django.urls import path
+from django.urls import re_path
 from django.shortcuts import render
 from .views.booking import booking
 from .views import booking
@@ -12,7 +12,11 @@ app_name = 'app'
 
 urlpatterns = [
     url(r'^$', home, name='home'),  # Test homepage
-    url(r'^shows_list', shows_list, name='listeshows'),
-    url(r'^shows/(?P<id_article>.+)', show_detail, name='show'),
+
+    #url(r'^shows_list', shows_list, name='listeshows'),
+    #url(r'^shows/(?P<id_article>.+)', show_detail, name='show'),
+    re_path(r'shows-index/$', shows_list.ListView.as_view(), name='shows-list'), # ShowList page
+    re_path(r'show/(?P<pk>[0-9]+)/$', show_detail.DetailView.as_view(), name='show-detail'), # ShowDetail page
+
     url(r'^booking', booking.booking, name='booking') 
 ]
--- reservations/app/views/shows_list.py
+++ reservations/app/views/shows_list.py
@@ -1,19 +1,20 @@
-from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
 
 from app.models import Show
 
-# Show ListView Class definitio
+"""# Show ListView Class definitio
 class Listshows(ListView):
-    """explication"""
+    explication
     model = Show
     template_name = 'app/shows/shows_list.html'
     context_object_name = 'shows'
     paginate_by = 10
 
     def get_context_data(self, **kwargs):
-        """def context"""
+        def context
         context = super(Listshows, self).get_context_data(**kwargs)
         context['title'] = "Liste des spectacles"
         return context
+"""
