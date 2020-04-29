0df0f86f0a6f1ca9b657d245b7be9975154c6c2a
Dieudonné <twahirwa25@gmail.com>
2020-04-30 00:48:49 +0200

    update user forms

--- reservations/app/forms/user.py
+++ reservations/app/forms/user.py
@@ -4,6 +4,9 @@ from django.contrib.auth.forms import (
     AuthenticationForm,
     UsernameField,
     UserCreationForm,
+    PasswordChangeForm,
+    ReadOnlyPasswordHashField,
+    UserChangeForm,
 )
 
 from app.models.user_profile import UserProfile
@@ -33,3 +36,45 @@ class UserRegisterForm(UserCreationForm):
             'password2',
         ]
 
+class UserUpdateForm(UserChangeForm):
+    first_name = forms.CharField(label="Prénom")
+    last_name = forms.CharField(label="Nom")
+    username = forms.CharField(label="Pseudo")
+    email = forms.EmailField(label="E-mail")
+    password = ReadOnlyPasswordHashField(
+        label="Mot de passe",
+        disabled=True,
+        widget=forms.PasswordInput(attrs={'placeholder':'**********'}),
+        help_text=  "Les mots de passe bruts ne sont pas stockés,\
+                    il n'y a donc aucun moyen de voir le mot de passe de cet utilisateur,\
+                    mais vous pouvez le changer en utilisant ce \
+                    <a href='/password-change/'>formulaire.</a>",
+    )
+
+    class Meta:
+        model = User
+        fields = [
+            'last_name',
+            'first_name',
+            'username',
+            'email',
+            
+        ]
+
+class UserProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = []
+
+class CustomPasswordChangeForm(PasswordChangeForm):
+    old_password = forms.CharField(label="Ancien mot de passe", widget=forms.PasswordInput)
+    new_password1 = forms.CharField(label="Nouveau mot de passe", widget=forms.PasswordInput)
+    new_password2 = forms.CharField(label=" Confirmation du nouveau mot de passe", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = [
+            'old_password',
+            'new_password1',
+            'new_password2',            
+        ]
