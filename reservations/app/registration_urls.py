70439812208c39e4cb88d07475a02b8af4dbc704
Dieudonné <twahirwa25@gmail.com>
2020-04-30 00:48:34 +0200

    update app urls : create profile url

--- reservations/app/registration_urls.py
+++ reservations/app/registration_urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     re_path(r'login/$', user.CustomLoginView.as_view(), name='login' ), #User login page
     re_path(r'^logout/', auth_views.LogoutView.as_view(template_name='app/user/logout.html'), name='logout'), #User logout page
     re_path(r'^register/', user.register, name='register'), #User registration page
+    re_path(r'^profile/', user.profile, name='profile'),
     re_path(r'^password-reset/done/',
             auth_views.PasswordResetDoneView.as_view(template_name='app/user/password_reset_done.html'),
             name='password_reset_done'), #User password reset done page
