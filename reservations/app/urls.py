ad0fde2d268213ac047d8c1f841b4a9601a93c88
Dieudonné <twahirwa25@gmail.com>
2020-04-29 21:30:06 +0200

    update app urls : add user password reset urls

--- reservations/app/urls.py
+++ reservations/app/urls.py
@@ -1,4 +1,4 @@
-from django.urls import re_path, include
+from django.urls import re_path, include, path
 from django.contrib.auth import views as auth_views
 
 from .views import views
@@ -11,7 +11,18 @@ urlpatterns = [
     re_path(r'login/$', user.CustomLoginView.as_view(), name='login' ), #User login page
     re_path(r'^logout/', auth_views.LogoutView.as_view(template_name='app/user/logout.html'), name='logout'), #User logout page
     re_path(r'^register/', user.register, name='register'), #User registration page
-
+    re_path(r'^password-reset/',
+            auth_views.PasswordResetView.as_view(template_name='app/user/password_reset.html'),
+            name='password_reset'), #User password reset page
+    re_path(r'^password-reset/done/',
+            auth_views.PasswordResetDoneView.as_view(template_name='app/user/password_reset_done.html'),
+            name='password_reset_done'), #User password reset done page
+    path('password-reset-confirm/<uidb64>/<token>/',
+            auth_views.PasswordResetConfirmView.as_view(template_name='app/user/password_reset_confirm.html'),
+            name='password_reset_confirm'), #User password reset confirm page
+    re_path(r'^password-reset/complete/',
+            auth_views.PasswordResetCompleteView.as_view(template_name='app/user/password_reset_complete.html'),
+            name='password_reset_complete'), #User password reset complete
 
     re_path(r'shows-index/$', show.ListView.as_view(), name='shows-list'), # ShowList page
     re_path(r'show/(?P<pk>[0-9]+)/$', show.DetailView.as_view(), name='show-detail'), # ShowDetail page
