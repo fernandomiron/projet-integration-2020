4c271adec5c75e5e90d7c315b68065ed04c4703c
Dieudonné <twahirwa25@gmail.com>
2020-04-30 00:16:08 +0200

    fix app registration_urls

--- reservations/app/registration_urls.py
+++ reservations/app/registration_urls.py
@@ -7,9 +7,6 @@ urlpatterns = [
     re_path(r'login/$', user.CustomLoginView.as_view(), name='login' ), #User login page
     re_path(r'^logout/', auth_views.LogoutView.as_view(template_name='app/user/logout.html'), name='logout'), #User logout page
     re_path(r'^register/', user.register, name='register'), #User registration page
-    re_path(r'^password-reset/',
-            auth_views.PasswordResetView.as_view(template_name='app/user/password_reset.html'),
-            name='password_reset'), #User password reset page
     re_path(r'^password-reset/done/',
             auth_views.PasswordResetDoneView.as_view(template_name='app/user/password_reset_done.html'),
             name='password_reset_done'), #User password reset done page
@@ -19,4 +16,7 @@ urlpatterns = [
     re_path(r'^password-reset/complete/',
             auth_views.PasswordResetCompleteView.as_view(template_name='app/user/password_reset_complete.html'),
             name='password_reset_complete'), #User password reset complete
+    re_path(r'^password-reset/',
+            auth_views.PasswordResetView.as_view(template_name='app/user/password_reset.html'),
+            name='password_reset'), #User password reset page
 ]
