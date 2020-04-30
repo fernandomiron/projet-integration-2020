2d474b854667f79fce414ea241b7efda4824c06d
Dieudonné <twahirwa25@gmail.com>
2020-04-30 15:15:20 +0200

    update app static : update js and css

--- reservations/app/static/app/js/custom.js
+++ reservations/app/static/app/js/custom.js
@@ -9,4 +9,14 @@ $(document).ready(function () {
     $("#menu-user").mouseleave(function () {
         dropDownMenu.slideUp(300)
     })
+
+    $("#Update_profil").click(function () {
+        $('form.hide').show(700);
+        $('#Update_profil').hide();
+    });
+
+    $('#Cancel_update_profil').click(function () {
+        $('#Update_profil').show();
+        $('form.hide').hide(700);
+    })
 });
