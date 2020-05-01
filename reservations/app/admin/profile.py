92ec767ffde63ef13d4986d3bac251daea21545d
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-05-02 00:42:48 +0200

    Add relative function of UserProfile to User
    Add function that creates a new UserProfile instance when a User instance is created.
    Add the same function when the User instance is saved.
    The UserProfile fields are appended to the User fields in the admin form view.

--- reservations/app/admin/profile.py
+++ reservations/app/admin/profile.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
 from app.models import UserProfile
 
 
-admin.site.register(UserProfile)
+class UserProfileInline(admin.StackedInline):
+    """Define an inline admin descriptor for UserProfile model """
+
+    model = UserProfile
+    can_delete = False
+
+
+class UserAdmin(BaseUserAdmin):
+    """Override of the default UserAdmin"""
+
+    inlines = (UserProfileInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- reservations/app/models/profile.py
+++ reservations/app/models/profile.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 LANGUAGES = [
@@ -36,4 +38,23 @@ class UserProfile(models.Model):
 
         return ('')  # TODO: Define absolute url + url name
 
-    # TODO: Define custom methods here
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """Creates a UserProfile instance when a User instance is created
+
+    The UserProfile instance is linked to the created User.
+    """
+
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    """Saves the UserProfile instance its User instance is saved
+
+    The UserProfile instance is linked to the created User.
+    """
+
+    instance.userprofile.save()
