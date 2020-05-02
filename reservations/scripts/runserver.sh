8a56c987b5a7e61bfb94d666a12c82fc0a81e41c
Likiol <lionel.soupart@gmail.com>
2020-05-02 14:13:01 +0200

    pull master + modelization

--- reservations/scripts/runserver.sh
+++ reservations/scripts/runserver.sh
@@ -4,7 +4,7 @@
 # This is for development purposes
 
 while true; do
-  if ! python3 manage.py runserver_plus 0.0.0.0:8000; then
+  if ! py manage.py runserver_plus 0.0.0.0:8000; then
     echo "\nrunserver_plus exited with a non 0 status: restarting in 5 seconds...\n"
     sleep 5;
   else
