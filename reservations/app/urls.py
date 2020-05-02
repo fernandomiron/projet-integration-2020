f356c90de02cb7f053457dcee98d6bf4efeacd3a
Likiol <lionel.soupart@gmail.com>
2020-05-02 16:37:59 +0200

    add api.view +url test

--- reservations/app/urls.py
+++ reservations/app/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
 from .views import views
-
+from .views.api import ApiGetView
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),  # Test homepage
+    url(r'^api', ApiGetView.as_view,name='api') # Vue API
 ]
---
+++ reservations/app/views/api.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+#third party imports
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class ApiGetView (APIView) :
+    def get(self,request, *args, **kwargs) :
+        data = {
+            'age' : 23
+        }
+        return Response(data)
