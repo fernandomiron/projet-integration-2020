24c4f99a255c6505e6d01c78bfcc4dfe0ac592bd
Dieudonné <twahirwa25@gmail.com>
2020-05-06 13:50:38 +0200

    update app admin profile : add UserProfileRessource class + UserProfileAdmin class

--- reservations/app/admin/profile.py
+++ reservations/app/admin/profile.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
 from app.models import UserProfile
 
+#  Describe how UserProfile resources can be imported or exported:
+class UserProfileResource(resources.ModelResource):
 
-admin.site.register(UserProfile)
+    class Meta:
+        model = UserProfile
+        skip_unchanged = True
+
+#  Admin integration 
+class UserProfileAdmin(ImportExportModelAdmin):
+   resource_class = UserProfileResource
+
+admin.site.register(UserProfile, UserProfileAdmin)
