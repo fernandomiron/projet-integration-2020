d13205256b84dfc7b593325b7a899f8b3159f183
Dieudonné <twahirwa25@gmail.com>
2020-05-06 03:23:37 +0200

    update app artist admin : add ArtistResource class + ArtistAdmin

--- reservations/app/admin/artist.py
+++ reservations/app/admin/artist.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 
+from import_export import resources
+from import_export.admin import ImportExportActionModelAdmin
+
 from app.models import Artist, Types
 
-admin.site.register(Artist)
+
+#  Describe how Artist resources can be imported or exported:
+class ArtistResource(resources.ModelResource):
+
+    class Meta:
+        model = Artist
+        import_id_fields = ('firstname',)
+        skip_unchanged = True
+    
+
+#  Admin integration 
+class ArtistAdmin(ImportExportActionModelAdmin):
+   resource_class = ArtistResource
+
+admin.site.register(Artist, ArtistAdmin)
 admin.site.register(Types)
+
