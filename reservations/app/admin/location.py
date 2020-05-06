dbbd4d165fbc4185f581a9b90b1bdb7a4f0ead2d
Dieudonné <twahirwa25@gmail.com>
2020-05-06 13:47:34 +0200

    update app admin location : add LocationRessource and LocalityRessource class + LocationAdmin and LocalityAdmin class

--- reservations/app/admin/location.py
+++ reservations/app/admin/location.py
@@ -1,7 +1,32 @@
 from django.contrib import admin
 
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
 from app.models import Location, Locality
 
 
-admin.site.register(Location)
-admin.site.register(Locality)
+#  Describe how Artist resources can be imported or exported:
+class LocationResource(resources.ModelResource):
+
+    class Meta:
+        model = Location
+        skip_unchanged = True
+
+#  Describe how Type resources can be imported or exported:
+class LocalityResource(resources.ModelResource):
+
+    class Meta:
+        model = Locality
+        skip_unchanged = True
+    
+
+#  Admin integration 
+class LocationAdmin(ImportExportModelAdmin):
+   resource_class = LocationResource
+
+class LocalityAdmin(ImportExportModelAdmin):
+   resource_class = LocalityResource
+
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Locality, LocalityAdmin)
