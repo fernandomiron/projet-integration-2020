89fc77fc4f16d044bf72b90b2e42d2ba388674d1
Dieudonné <twahirwa25@gmail.com>
2020-05-06 13:49:16 +0200

    update app admin show : add ShowRessource and RepresentationRessource class + ShowAdmin and RepresentationAdmin class

--- reservations/app/admin/show.py
+++ reservations/app/admin/show.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
 
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
 from app.models import Representation, Show
 
-admin.site.register(Show)
-admin.site.register(Representation)
+#  Describe how Representation resources can be imported or exported:
+class RepresentationResource(resources.ModelResource):
+
+    class Meta:
+        model = Representation
+        skip_unchanged = True
+
+#  Describe how Type resources can be imported or exported:
+class ShowResource(resources.ModelResource):
+
+    class Meta:
+        model = Show
+        skip_unchanged = True
+    
+
+#  Admin integration 
+class RepresentationAdmin(ImportExportModelAdmin):
+   resource_class = RepresentationResource
+
+class ShowAdmin(ImportExportModelAdmin):
+   resource_class = ShowResource
+
+admin.site.register(Show, ShowAdmin)
+admin.site.register(Representation, RepresentationAdmin)
