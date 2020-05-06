ab6895c08938454385a2b2e0996ea15cbc89ea38
Dieudonné <twahirwa25@gmail.com>
2020-05-06 13:45:08 +0200

    update app admin reservation : add ReservationRessource class + ReservationnAdmin class

--- reservations/app/admin/reservation.py
+++ reservations/app/admin/reservation.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
 from app.models import Reservation
 
+#  Describe how Reservation resources can be imported or exported:
+class ReservationResource(resources.ModelResource):
 
-admin.site.register(Reservation)
+    class Meta:
+        model = Reservation
+        skip_unchanged = True
+
+#  Admin integration 
+class ReservationAdmin(ImportExportModelAdmin):
+   resource_class = ReservationResource
+
+admin.site.register(Reservation, ReservationAdmin)
