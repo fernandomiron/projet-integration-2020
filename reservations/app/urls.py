3093726d2751b7a686339f4e5ef0b99b666bfb5d
Fernando Miron <fernando.miron@gmail.com>
2020-05-07 23:02:46 +0200

    password reset mail working, need to set the form

--- reservations/app/urls.py
+++ reservations/app/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from django.contrib.auth import views as auth_views # import of the built-in Django authentication and give it a "auth_views" alias
 from django.urls import path
 
-
+# import of custom made views
 from .views import views
 
 
@@ -10,4 +10,8 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),  # Test homepage
     url(r'^login/$', auth_views.LoginView.as_view(template_name = "app/login.html"), name='login'), #login url using LoginView built-in authentication
     url(r'^logout/$', auth_views.LogoutView.as_view(template_name = "app/logout.html"), name='logout'),#logout url using LogoutView built-in authentication
+    url(r'password-reset/$', auth_views.PasswordResetView.as_view(template_name = "app/password_reset_form.html"), name='password_reset'), # password reset url
+    url(r'password-reset/done/$', auth_views.PasswordResetDoneView.as_view(template_name = "app/password_reset_done.html"), name='password_reset_done'),#password reset done url
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.PasswordResetConfirmView.as_view(template_name = "app/password_reset_confirm.html"), name='password_reset_confirm'), # password reset confirm url
+    url(r'reset/done/$', auth_views.PasswordResetCompleteView.as_view(template_name = "app/password_reset_complete.html"), name='password_reset_complete'),# password reset complete url
 ]
