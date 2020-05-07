35996d9b4c7a97ccbbb41a83501d4dd8f396387f
dreamer_658 <calebyambu@yahoo.fr>
2020-05-07 16:55:11 +0200

    fixing bugs on locationdetailed views

--- reservations/app/admin/admin.py
+++ reservations/app/admin/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+
+from .show import Show
+
+class ReservationAdmin (admin.ModelAdmin):
+    list_display = ('title', 'slug', 'poster', 'price')
+
+admin.site.register(Show )
--- reservations/app/views/locationList.py
+++ reservations/app/views/locationList.py
@@ -10,11 +10,11 @@ def LocationListView(request):
     search_term = ''
 
     if 'search' in request.GET:
-        search_term = request.GET['search']
+        search_term = request.GET['search']#getting the searched element in the url
         locations = locations.filter(designation__icontains=search_term)
 
     paginator = Paginator(locations,5)
-    #pagination will allow only 4 items.
+    #pagination will allow only 5 items.
     page = request.GET.get('page')
     locations = paginator.get_page(page)
     return render(request, 'app/locationList.html',{'locations':locations, 'search_term': search_term })
