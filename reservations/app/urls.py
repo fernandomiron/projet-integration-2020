6cdcbe40b65752082072c28ae5df8c0f69abb113
Likiol <lionel.soupart@gmail.com>
2020-05-09 14:53:40 +0200

    url api artist modified

--- reservations/app/urls.py
+++ reservations/app/urls.py
@@ -5,6 +5,7 @@ from .views.api import ArtistApiView
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),  # Test homepage
-    url(r'^api/', ArtistApiView.as_view(),name='api')
+    url(r'^api/Artist/', ArtistApiView.as_view(),name='api')
+    #url(r'^api/Artist/(?P<lastname>)/$', ArtistApiView.as_view(),name='api')
     #url(r'^api/', ApiGetView.as_view(),name='api') # Vue API
 ]
