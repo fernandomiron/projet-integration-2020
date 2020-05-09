08ea1fc48faa7942f61fe3c2fd82dd1b9074023e
Likiol <lionel.soupart@gmail.com>
2020-05-09 15:20:28 +0200

    add parameters for api url artist

--- reservations/app/urls.py
+++ reservations/app/urls.py
@@ -6,5 +6,5 @@ from .views.api import ArtistApiView
 urlpatterns = [
     url(r'^$', views.home, name='home'),  # Test homepage
     url(r'^api/Artist/', ArtistApiView.as_view(),name='apiArtist'),
-    url(r'^api/Artist/(?P<lastname>[a-z])/$', ArtistApiView.as_view(),name='apiArtistname')
+    
 ]
