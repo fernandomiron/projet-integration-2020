30de3518b985ac816284834d927e1a9edec878fd
Likiol <lionel.soupart@gmail.com>
2020-05-09 13:12:11 +0200

    add url flux rss

--- reservations/app/urls.py
+++ reservations/app/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
 from .views import views
-from .feedrss import LastShowFeed
+from .feedrss import LastShowFeed, RepresentationFeed
 
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),  # Test homepage
-    url(r'^showrss/',LastShowFeed(), name='showrss'),
+    url(r'^showrss/',LastShowFeed(), name='showrss')
+    url(r'^representationrss/',RepresentationFeed(), name='representationrss'),
 ]
