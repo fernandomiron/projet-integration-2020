227b1048d3424ea637bb0c715dd4b765cf6439a4
Likiol <lionel.soupart@gmail.com>
2020-05-09 16:02:32 +0200

    add url representationapiview

--- reservations/app/urls.py
+++ reservations/app/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
 from .views import views
-from .views.api import ArtistApiView
+from .views.api import ArtistApiView, representationApiView
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),  # Test homepage
-    url(r'^api/Artist/', ArtistApiView.as_view(),name='apiArtist'),
+    url(r'^api/artist/', ArtistApiView.as_view(),name='apiArtist'),
+    url(r'^api/representation/',representationApiView.as_view(), name='apirepresentation'),
     
 ]
