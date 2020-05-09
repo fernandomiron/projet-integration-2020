f9924080d21072a3b5d8a7be879ae69eb7954c41
Likiol <lionel.soupart@gmail.com>
2020-05-09 16:03:16 +0200

    add comment url

--- reservations/app/urls.py
+++ reservations/app/urls.py
@@ -5,7 +5,7 @@ from .views.api import ArtistApiView, representationApiView
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),  # Test homepage
-    url(r'^api/artist/', ArtistApiView.as_view(),name='apiArtist'),
-    url(r'^api/representation/',representationApiView.as_view(), name='apirepresentation'),
+    url(r'^api/artist/', ArtistApiView.as_view(),name='apiArtist'), #view api artist
+    url(r'^api/representation/',representationApiView.as_view(), name='apirepresentation'),# view api representation
     
 ]
