da116afc3c77282bd9eb9501e7ba98cf43d17ad2
Likiol <lionel.soupart@gmail.com>
2020-05-09 14:26:46 +0200

    old class as comment

--- reservations/app/views/api.py
+++ reservations/app/views/api.py
@@ -9,23 +9,23 @@ from rest_framework.response import Response
 from app.serializers.artists import ArtistSerializer
 from app.models.artist import Artist
 
-class ApiGetView (APIView) :
+#class ApiGetView (APIView) :
 
     #permission_classes = (IsAuthenticated)
     #define who can access to the class
 
-    def get(self, request, *args, **kwargs):
-        qsArtist = Artist.objects.all()
+    #def get(self, request, *args, **kwargs):
+        #qsArtist = Artist.objects.all()
 
-        serializer = ArtistSerializer(qsArtist, many=True)
+        #serializer = ArtistSerializer(qsArtist, many=True)
 
-        return Response(serializer.data)
+        #return Response(serializer.data)
 
-    def post(self, request, *args, **kwargs):
-        serializer = ArtistSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response (serializer.data)
-        return Response(serializer.errors)
+    #def post(self, request, *args, **kwargs):
+        #serializer = ArtistSerializer(data=request.data)
+        #if serializer.is_valid():
+            #serializer.save()
+            #return Response (serializer.data)
+        #return Response(serializer.errors)
 
 
