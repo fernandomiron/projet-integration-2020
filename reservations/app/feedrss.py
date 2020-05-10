3762bc1e47bd8b0be098d090c6036d56fb8a5609
Likiol <lionel.soupart@gmail.com>
2020-05-10 19:47:38 +0200

    add class LocationFeed

--- reservations/app/feedrss.py
+++ reservations/app/feedrss.py
@@ -1,6 +1,6 @@
 from django.contrib.syndication.views import Feed
 from django.urls import reverse
-from app.models import Show, Representation
+from app.models import Show, Representation, Location
 
 # Show the 2 last shows
 class LastShowFeed(Feed):
@@ -25,6 +25,21 @@ class RepresentationFeed(Feed):
 
     def items(self):
         return Representation.objects.order_by('location')
+
+    def item_title(self,item):
+        return item.title
+
+    def item_description(self, item):
+        return item.description
+
+#show location
+class LocationFeed(Feed):
+    title = "Localisations "
+    link = "/locationrss/"
+    description = "All location"
+
+    def items(self):
+        return Location.objects.order_by('locality')
 
     def item_title(self,item):
         return item.title
