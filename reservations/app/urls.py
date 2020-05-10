f4266c1e306342a5cd02ae6c6495a365c317fa40
Likiol <lionel.soupart@gmail.com>
2020-05-10 18:58:49 +0200

    add url showapi

--- reservations/app/urls.py
+++ reservations/app/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 
 from .views import views
-from .views.api import ArtistApiView, RepresentationApiView
+from .views.api import ArtistApiView, RepresentationApiView, ShowApiView
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),  # Test homepage
     url(r'^api/artist/', ArtistApiView.as_view(),name='apiArtist'), #view api artist
     url(r'^api/representation/',RepresentationApiView.as_view(), name='apirepresentation'),# view api representation
-    
+    url(r'^api/show/',ShowApiView.as_view(), name='apishow'),# view api show
 ]
