bd33c0932cba89220f3fca51ecd7d37f358d5379
Dieudonné <twahirwa25@gmail.com>
2020-05-13 15:15:47 +0200

    update app urls : add user profil-update url

--- reservations/app/urls.py
+++ reservations/app/urls.py
@@ -12,7 +12,8 @@ urlpatterns = [
     url(r'^signup/$', authentication.signup, name='signup'), # signup/register url for user registration
     url(r'^login/$', auth_views.LoginView.as_view(template_name = "app/login.html"), name='login'), #login url using LoginView built-in authentication
     url(r'^logout/$', auth_views.LogoutView.as_view(template_name = "app/logout.html"), name='logout'),#logout url using LogoutView built-in authentication
-    url(r'profile/$', authentication.ProfileView.as_view(), name='profile'), # user ProfileView url   
+    url(r'profile/$', authentication.ProfileView.as_view(), name='profile'), # user ProfileView url
+    url(r'profile/update$', authentication.profileUpdate, name='profile-update'), # user ProfileView url    
     url(r'password-reset/$', auth_views.PasswordResetView.as_view(template_name = "app/password_reset_form.html"), name='password_reset'), # password reset url
     url(r'password-reset/done/$', auth_views.PasswordResetDoneView.as_view(template_name = "app/password_reset_done.html"), name='password_reset_done'),#password reset done url
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.PasswordResetConfirmView.as_view(template_name = "app/password_reset_confirm.html"), name='password_reset_confirm'), # password reset confirm url
