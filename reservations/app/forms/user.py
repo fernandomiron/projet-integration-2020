d864cae62ba7e6d3ba09b6c32db641abf72222f8
Dieudonné <twahirwa25@gmail.com>
2020-05-13 15:13:18 +0200

    update app forms : add UserUpdateForm and UserProfileUpdateForm + import UserCreationForm and UserChangeForm

--- reservations/app/forms/user.py
+++ reservations/app/forms/user.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm 
 
 from app.models import UserProfile # import of UserProfile
 
@@ -25,6 +25,30 @@ class UserSignupForm(UserCreationForm):
 UserProfileSignupForm custom made class
 '''
 class UserProfileSignupForm(forms.ModelForm):
+
+    class Meta:
+        model = UserProfile
+        fields = ['language']
+
+'''
+UserUpdateForm custom made class
+'''
+
+class UserUpdateForm(UserChangeForm):
+
+    class Meta:
+        model = User
+        fields = [
+            'last_name',
+            'first_name',
+            'username',
+            'email',
+        ]
+
+'''
+UserProfileUpdateForm custom made class
+'''
+class UserProfileUpdateForm(forms.ModelForm):
 
     class Meta:
         model = UserProfile
