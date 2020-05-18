90705e1f060f2b592f26f6052d064d325fb47a8f
Likiol <lionel.soupart@gmail.com>
2020-05-18 21:09:12 +0200

    update installed apps

--- reservations/reservations/settings.py
+++ reservations/reservations/settings.py
@@ -50,7 +50,8 @@ INSTALLED_APPS = [
     'app',  # Project app module
     'import_export',  # django-import-export==2.1.0
     'rest_framework',  # djangorestframework==3.11.0
-    'url_filter'  # django-url-filter==0.3.15
+    'url_filter',  # django-url-filter==0.3.15
+    'paypal.standard.ipn', #django-paypal==1.0.0
 
 ]
 
