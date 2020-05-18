e4b6664374396c4e9ec7bc7f5ff7f39d080c5bf2
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-05-18 02:42:18 +0200

    PEP8 formatting

--- reservations/app/views/api.py
+++ reservations/app/views/api.py
@@ -1,40 +1,35 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
-#third party imports
-from rest_framework.permissions import IsAdminUser, IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import generics
-
-#don't forget adding url_filter in settings
+from rest_framework.permissions import (
+    AllowAny, IsAdminUser, IsAuthenticated, IsAuthenticatedOrReadOnly)
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from url_filter.integrations.drf import DjangoFilterBackend
 
-#information import about Artist
-from app.serializers.artists import ArtistSerializer
 from app.models.artist import Artist
-
-#informations import about show
-from app.serializers.show import ShowSerializer
-from app.models.show import Show
-
-#informations import about Representation
-from app.serializers.representation import RepresentationSerializer
-from app.models.show import Representation
-
-#informations import about Location
-from app.serializers.location import LocationSerializer
 from app.models.location import Location
+from app.models.show import Representation
+from app.models.show import Show
+from app.serializers.artists import ArtistSerializer
+from app.serializers.location import LocationSerializer
+from app.serializers.representation import RepresentationSerializer
+from app.serializers.show import ShowSerializer
 
 
 class ArtistApiView (generics.ListAPIView):
+    """ Comment here """  # TODO: Comment class
 
     queryset = Artist.objects.all()
     serializer_class = ArtistSerializer
     filter_backends = [DjangoFilterBackend]
-    filter_fields = ('id','lastname')
+    filter_fields = ('id', 'lastname')
+
 
 class RepresentationApiView (generics.ListAPIView):
+    """ Comment here """  # TODO: Comment class
+
     queryset = Representation.objects.all()
     serializer_class = RepresentationSerializer
     filter_backends = [DjangoFilterBackend]
@@ -42,12 +37,17 @@ class RepresentationApiView (generics.ListAPIView):
 
 
 class ShowApiView (generics.ListAPIView):
+    """ Comment here """  # TODO: Comment class
+
     queryset = Show.objects.all()
     serializer_class = ShowSerializer
     filter_backends = [DjangoFilterBackend]
     filter_fields = ('id', 'title')
 
+
 class LocationApiView (generics.ListAPIView):
+    """ Comment here """  # TODO: Comment class
+
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
     filter_backends = [DjangoFilterBackend]
