01abc4bed0a45485597ac3372fa1e7d5dc5a19fb
Fernando Miron <fernando.miron@gmail.com>
2020-05-24 19:52:11 +0200

    fixed url problem with profile/update and extension to base.html for same template

--- reservations/app/urls.py
+++ reservations/app/urls.py
@@ -36,9 +36,7 @@ urlpatterns = [
         name='profile-update'),
 
     # Password Change
-    url(r'^password/change/$',
-        auth_views.PasswordChangeView.as_view(template_name="app/\
-            password_change_form.html"), name='password_change_form'),
+    url(r'^password/change/$',auth_views.PasswordChangeView.as_view(template_name="app/password_change_form.html"), name='password_change_form'),
     url(r'^password/change/done/$',
         auth_views.PasswordChangeDoneView.as_view(template_name="app/\
             password_change_done.html"), name='password_change_done'),
