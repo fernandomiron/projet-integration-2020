a5ff84bc4f2b5d2048fd4dfd7835b489bd34cfa4
Dieudo25 <twahirwa25@gmail.com>
2020-05-24 18:36:21 +0200

    update app view api : delete api_detail_show_view, api_update_show_view, api_delele_show_view, api_create_show_view

--- reservations/app/views/api.py
+++ reservations/app/views/api.py
@@ -8,7 +8,6 @@ from rest_framework.permissions import (
     AllowAny, IsAdminUser, IsAuthenticated, IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view
 from url_filter.integrations.drf import DjangoFilterBackend
 
 from app.models.artist import Artist
@@ -129,66 +128,3 @@ class ExternalAPI(generics.ListAPIView):
                     attempt_num += 1
                     time.sleep(5)  # Wait for 5 seconds before re-trying
             return Response({"error": "Request failed"}, status=r.status_code)
-
-
-
-from rest_framework.decorators import api_view, permission_classes
-
-@api_view(['GET'])
-def api_detail_show_view(request, slug):
-    
-    try:
-        show = Show.objects.get(slug=slug)
-    except Show.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == 'GET':
-        serializer = ShowSerializer(show)
-        return Response(serializer.data)
-
-@api_view(['PUT'])
-def api_update_show_view(request, slug):
-    
-    try:
-        show = Show.objects.get(slug=slug)
-    except Show.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == 'PUT':
-        serializer = ShowSerializer(show, data=request.data)
-        data = {}
-        if serializer.is_valid():
-            serializer.save()
-            data["success"] = "Update successful"
-            return Response(data=data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['DELETE'])
-def api_delele_show_view(request, slug):
-    
-    try:
-        show = Show.objects.get(slug=slug)
-    except Show.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == 'DELETE':
-        operation = show.delete()
-        data = {}
-        if operation:
-            data["success"] = "delete successful"
-        else:
-            data["failure"] = "delete failed"
-        return Response(data=data)
-
-@api_view(['POST', ])
-@permission_classes((IsAuthenticated,))
-def api_create_show_view(request):
-    
-    if request.method == 'POST':
-        serializer = ShowSerializer(Show(), data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
