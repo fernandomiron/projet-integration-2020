16c4c8adc434e9343c1a4c7fea5629116c46cfc7
Fernando Miron <fernando.miron@gmail.com>
2020-05-24 21:00:17 +0200

    fixed all urls on authapp + test functionality

--- reservations/app/urls.py
+++ reservations/app/urls.py
@@ -40,19 +40,10 @@ urlpatterns = [
     url(r'^password/change/done/$',auth_views.PasswordChangeDoneView.as_view(template_name="app/password_change_done.html"), name='password_change_done'),
 
     # Password Reset
-    url(r'^password/reset/$',
-        auth_views.PasswordResetView.as_view(template_name="app/\
-            password_reset_form.html"), name='password_reset'),
-    url(r'^password/reset/done/$',
-        auth_views.PasswordResetDoneView.as_view(template_name="\
-            app/password_reset_done.html"), name='password_reset_done'),
-    url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/\
-        (?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.PasswordResetConfirmView.as_view(template_name="app/\
-            password_reset_confirm.html"), name='password_reset_confirm'),
-    url(r'^password/reset/done/$',
-        auth_views.PasswordResetCompleteView.as_view(template_name="app/\
-            password_reset_complete.html"), name='password_reset_complete'),
+    url(r'^password/reset/$',auth_views.PasswordResetView.as_view(template_name="app/password_reset_form.html"), name='password_reset'),
+    url(r'^password/reset/done/$',auth_views.PasswordResetDoneView.as_view(template_name="app/password_reset_done.html"), name='password_reset_done'),
+    url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',auth_views.PasswordResetConfirmView.as_view(template_name="app/password_reset_confirm.html"), name='password_reset_confirm'),
+    url(r'^reset/complete/$',auth_views.PasswordResetCompleteView.as_view(template_name="app/password_reset_complete.html"), name='password_reset_complete'),
 
     # Shows
     url(r'^show/$', show.show_list, name='show'),
