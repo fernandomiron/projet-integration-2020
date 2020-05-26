960e8145c66bda640d40c5a1ce1a8b5e662107ea
cedric <cndjoni@gmail.com>
2020-05-26 23:48:09 +0200

    fix reservation view create reservation

--- reservations/app/views/reservation.py
+++ reservations/app/views/reservation.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt # permet de selectionner si on prend un post ou un get en fonction de la vue? 
 from paypal.standard.forms import PayPalEncryptedPaymentsForm, PayPalPaymentsForm
-from app.models import *
+from app.models.reservation import Reservation
 from django.conf import settings
 from django.contrib import messages
 from decimal import Decimal
@@ -23,17 +23,9 @@ def reservationglobalview(request):
 
 def reservationview(request,pk):
 
-    
-    reservation = Reservation.objects.get(pk=pk)
-    qty = request.GET.get('qty')
-    if qty == '':
-        reservation.seats = 1
-    else:
-        reservation.seats=float(qty)
-    
-    reservation.save()
-    # print(reservation)
-
+    qty = float(request.GET.get('qty', default=1))
+    representation = get_object_or_404(Representation, pk=pk)
+    reservation = Reservation.objects.create(representation=representation, user=request.user, seats=qty)
     context = {"reservation": reservation}
     template = "app/reservationview.html"
 
