cccbaa774075e4687282e378429edfe995f748f8
Dieudo25 <twahirwa25@gmail.com>
2020-05-28 15:43:36 +0200

    update app.views.show : fix update_show_external_api and show_external_api api_url variable

--- reservations/app/views/show.py
+++ reservations/app/views/show.py
@@ -62,9 +62,9 @@ def show_detail_slug(request, slug):
 
 def show_external_api(request):
 
-    api_url = "http://127.0.0.1:8000/api/show/pull/"
-    r = requests.get(api_url, timeout=10)
-    data = r.json()
+    api_url = "http://127.0.0.1:8000/" + reverse('ext-api-show')
+    response = requests.get(api_url, timeout=10)
+    data = response.json()
 
     context = {
         'data' : data
@@ -74,14 +74,14 @@ def show_external_api(request):
 
 def update_show_external_api(request):
 
-    api_url = "http://127.0.0.1:8000/api/show/pull/"
-    r = requests.get(api_url, timeout=10)
-    data = r.json()
+    api_url = "http://127.0.0.1:8000/" + reverse('ext-api-show')
+    response = requests.get(api_url, timeout=10)
+    data = response.json()
     data_to_create = []
     data_to_update = []
 
     for show in data:
-        if show['description'] == None:
+        if show['description'] is None:
             show['description'] = 'Pas de description'
         new_show = Show(
             title = show['title'],
