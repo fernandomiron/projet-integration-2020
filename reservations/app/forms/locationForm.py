c397c187ae764e7d30501c24432f8b6479bc029c
Fernando Miron <fernando.miron@gmail.com>
2020-06-02 21:06:20 +0200

    added forms comments

--- reservations/app/forms/locationForm.py
+++ reservations/app/forms/locationForm.py
@@ -2,6 +2,8 @@ from django import forms
 from app.models.location import Location
 
 class LocationForm(forms.ModelForm):
+    """ Location form """
+
     class Meta:
         model = Location
         fields = ['designation', 'address','locality','phone','website']
--- reservations/app/forms/showForm.py
+++ reservations/app/forms/showForm.py
@@ -2,6 +2,8 @@ from django import forms
 from app.models.show import Show
 
 class ShowForm(forms.ModelForm):
+    """ Show Form """
+
     class Meta:
         model = Show
         fields = ['title','poster','price','description','bookable' ]
